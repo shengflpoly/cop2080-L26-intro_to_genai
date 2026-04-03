814864d05a069ac6f620dc8c50588abb3e193d3d
christian navarro <cnavarro@floridapoly.edu>
2026-04-02 21:19:30 -0400

    Add placeholders for LLM integration and user input
    Added placeholders for future implementation and user input handling.

--- src/prompt_cls.py
+++ src/prompt_cls.py
@@ -1,5 +1,10 @@
 # prompt_cls.py
+## You will need to replace all #PLACEHOLDER# lines  
 from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Define a prompt template with placeholders
 template = """
@@ -13,8 +18,26 @@ prompt = PromptTemplate(
     input_variables=["text"],  # placeholders in the template
     template=template
 )
+# Initialize the Gemini LLM
+#PLACEHOLDER#
 
-# Example 
-filled_prompt = prompt.format(text="Utilizing chatbots and AI agents that react in real time to deliver more accurate and personalized responses using your own datasets. \
-Synthesizing data to understand customer profiles and generating content to reach target customers.")
-print(filled_prompt)
+#Function to get user input and generate response
+def chat_to_llm():
+    while True:
+       # collect user_input or quit to quit (check lower case )
+        #PLACEHOLDER#
+        #PLACEHOLDER#
+            break
+        # Format the prompt with user input
+        #PLACEHOLDER#
+        # Get response from LLM
+       #PLACEHOLDER#
+        print("Summary:", response.content)        
+
+if __name__ =="__main__":
+    chat_to_llm()
+
+# Previous Example COde
+#filled_prompt = prompt.format(text="Utilizing chatbots and AI agents that react in real time to deliver more accurate and personalized responses using your own datasets. \
+#Synthesizing data to understand customer profiles and generating content to reach target customers.")
+#print(filled_prompt)
